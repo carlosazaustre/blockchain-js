7bed2bf8926185eb4e9d303fa1002bb4a0968f1a
Samu Garron DEV <samugarrondev@gmail.com>
2022-03-29 19:39:18 -0300

    Convirtiendo data a exadecimal

--- src/block.js
+++ src/block.js
@@ -5,7 +5,7 @@ class Block {
   constructor(data) {
     this.hash = null;
     this.height = 0;
-    this.body = Buffer.from(JSON.stringify(data).toString("hex"));
+    this.body = Buffer.from(JSON.stringify(data)).toString("hex");
     this.time = 0;
     this.previousBlockHash = null;
   }
